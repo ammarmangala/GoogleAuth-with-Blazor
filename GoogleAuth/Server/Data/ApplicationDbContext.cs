252b4f7c129535ef3a844106f115216eabbc0e45
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2022-07-13 04:05:11 +0200

    Add project files.

---
+++ GoogleAuth/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using GoogleAuth.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace GoogleAuth.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ GoogleAuth/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GoogleAuth.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
